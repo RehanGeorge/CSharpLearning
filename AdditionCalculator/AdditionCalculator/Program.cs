2ee5237abd6b1bc74e98ebd342ebad4a76e74a73
Rehan George <rehan.george@gmail.com>
2025-05-13 12:52:45 +0530

    First commit

---
+++ AdditionCalculator/AdditionCalculator/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace AdditionCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int number1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Second Number");
+            int number2 = Int32.Parse(Console.ReadLine());
+
+            int number3 = number1 + number2;
+            Console.WriteLine("The sum is: " + number3);
+        }
+    }
+}
