36b9ca6d94710828d9d1c576287d11a390b00183
Rehan George <rehan.george@gmail.com>
2025-05-15 02:03:28 +0530

    Small quiz

--- LearningSnippets/LearningSnippets/Program.cs
+++ LearningSnippets/LearningSnippets/Program.cs
@@ -68,7 +68,34 @@
                 Console.WriteLine($"The month name is {monthName}");
             }
 
-            SwitchFunction();
+            static void QuizApp()
+            {
+                string question1 = "What is the capital of Germany?";
+                string answer1 = "Berlin";
+
+                string question2 = "What is 2+2?";
+                string answer2 = "4";
+
+                string question3 = "What color do you get by mixing blue and yellow?";
+                string answer3 = "Green";
+
+                int score = 0;
+                
+                Console.WriteLine(question1);
+                string userAnswer1 = Console.ReadLine();
+
+                if (userAnswer1 == answer1)
+                {
+                    Console.WriteLine("Correct!");
+                    score = score + 1;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong, the correct answer is: " + answer1);
+                }
+            }
+
+            QuizApp();
         }
     }
 }
