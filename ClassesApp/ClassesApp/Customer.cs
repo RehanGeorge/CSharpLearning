e4f58d2964dfe04f148409f9d8169aeba50305c9
Rehan George <rehan.george@gmail.com>
2025-05-17 20:20:17 +0530

    Create Customer class

---
+++ ClassesApp/ClassesApp/Customer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesApp
+{
+    internal class Customer
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string ContactNumber { get; set; }
+
+        // Default Customer
+        public Customer()
+        {
+            Name = "DefaultName";
+            Address = "No Address";
+            ContactNumber = "No Contact Number";
+        }
+
+        // Custom Constructor
+        public Customer(string name, string address, string contactNumber)
+        {
+            Name = name;
+            Address = address;
+            ContactNumber = contactNumber;
+        }
+
+        public Customer(string name)
+        {
+            Name = name;
+        }
+    }
+}
--- ClassesApp/ClassesApp/Program.cs
+++ ClassesApp/ClassesApp/Program.cs
@@ -6,12 +6,23 @@ namespace ClassesApp
     {
         static void Main(string[] args)
         {
-            Car audi = new Car("A3", "Audi", false);
-            Car bmw = new Car("i7", "BMW", true);
+            //Car audi = new Car("A3", "Audi", false);
+            //Car bmw = new Car("i7", "BMW", true);
 
-            // Getting Brand
-            Console.WriteLine($"Brand is " + audi.Brand);
-            Console.WriteLine($"Brand is " + bmw.Brand);
+            //// Getting Brand
+            //Console.WriteLine($"Brand is " + audi.Brand);
+            //Console.WriteLine($"Brand is " + bmw.Brand);
+
+            Customer earl = new Customer("Earl");
+            Customer frankTheTank = new Customer("FrankTheTank", "Mainstreet 1", "5551234556");
+
+            Console.WriteLine("Name of Customer is " + earl.Name);
+            // Default Customer with no Arguments given
+            Customer myCustomer = new Customer();
+            
+            Console.WriteLine("Details about customer: " + myCustomer.Name);
+            myCustomer.Name = "Jimbo";
+            Console.WriteLine(myCustomer.Name);
         }
     }
 }
