3fa7133f0c174afd1002ab4f546640626a1dfc2a
Rehan George <rehan.george@gmail.com>
2025-05-17 19:26:53 +0530

    Initial commit

---
+++ ClassesApp/ClassesApp/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesApp
+{
+    internal class Car
+    {
+        public Car()
+        {
+            Console.WriteLine("An object of car has been created.");
+        }
+    }
+}
