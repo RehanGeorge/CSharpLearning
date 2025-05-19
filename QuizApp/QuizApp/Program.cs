e4125d4cb6eaca7bf5ab718ba3fe32880e714549
Rehan George <rehan.george@gmail.com>
2025-05-19 20:25:56 +0530

    Quiz App with one question

--- QuizApp/QuizApp/Program.cs
+++ QuizApp/QuizApp/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Question[] questions = new Question[]
+            {
+                new Question("What is the capital of Germany?",
+                new string[]
+                {
+                    "Paris", "Berlin", "London", "Madrid"
+                }, 1)
+            };
+
+            Quiz myQuiz = new Quiz(questions);
+            myQuiz.DisplayQuestion(questions[0]);
+            myQuiz.DisplayAnswers(questions[0]);
+
+            int userChoice = int.Parse(Console.ReadLine()) - 1;
+            bool correct = questions[0].IsCorrectAnswer(userChoice);
+            Console.WriteLine(correct);
         }
     }
 }
---
+++ QuizApp/QuizApp/Quiz.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizApp
+{
+    internal class Quiz
+    {
+        private Question[] questions;
+
+        public Quiz(Question[] questions)
+        {
+            this.questions = questions;
+        }
+
+        public void DisplayQuestion(Question question)
+        {
+            Console.WriteLine(question.QuestionText);
+        }
+
+        public void DisplayAnswers(Question question)
+        {
+            int i = 0;
+            foreach (string answer in question.Answers)
+            {
+                Console.WriteLine($"{++i}. {answer}");
+            }
+        }
+    }
+}
