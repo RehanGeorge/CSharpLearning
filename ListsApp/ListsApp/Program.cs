87a755beaf1c3419eabf76ba4641469e83c9ce01
Rehan George <rehan.george@gmail.com>
2025-05-21 12:43:43 +0530

    Add lists

--- ListsApp/ListsApp/Program.cs
+++ ListsApp/ListsApp/Program.cs
@@ -8,23 +8,39 @@ namespace ListsApp
         {
             // Declaring a list and initializing
             List<string> colors = ["red", "blue", "green", "red"];
+            List<int> numbers = new List<int> { 10, 5, 15, 3, 9,  25, 18 };
 
-            printColors(colors);
+            Console.WriteLine("Sorting numbers");
+            numbers.Sort();
+
+            printItems(numbers);
+
+            Predicate<int> isGreaterThanTen = x => x >= 10;
+
+            Console.WriteLine("Here is the list of numbers greater than 10");
+            List<int> higherNumbers = numbers.FindAll(isGreaterThanTen);
+            printItems(higherNumbers);
+
+            bool hasLargeNumber = numbers.Any(x => x > 20);
+            Console.WriteLine($"There are large numbers in the numbers list: {hasLargeNumber}");
+
+            Console.WriteLine("Printing colors");
+            printItems(colors);
 
             while (colors.Remove("red") == true)
             {
                 Console.WriteLine("Deleted 1 red");
             }
 
-            printColors(colors);
+            printItems(colors);
         }
 
-        private static void printColors(List<string> colors)
+        private static void printItems<T>(List<T> items)
         {
-            Console.WriteLine("Current colors in the colors list!");
-            foreach (string color in colors)
+            Console.WriteLine("\nCurrent items in the list:");
+            foreach (T item in items)
             {
-                Console.WriteLine(color);
+                Console.WriteLine(item);
             }
         }
     }
