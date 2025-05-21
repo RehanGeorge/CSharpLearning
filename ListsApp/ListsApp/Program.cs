bc9d6eef146f398c5830a5bbcc3bdef8f97eae42
Rehan George <rehan.george@gmail.com>
2025-05-21 11:44:23 +0530

    Add colors to list

--- ListsApp/ListsApp/Program.cs
+++ ListsApp/ListsApp/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace ListsApp
+﻿using System.Drawing;
+
+namespace ListsApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Declaring a list and initializing
+            List<string> colors = new List<string>();
+
+            colors.Add("red");
+            colors.Add("blue");
+            colors.Add("green");
+
+            Console.WriteLine("Current colors in the colors list!");
+            foreach (string color in colors)
+            {
+                Console.WriteLine(color);
+            }
         }
     }
 }
