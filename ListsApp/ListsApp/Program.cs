cbf3c80538a571b81d7ae57d6f46c27966b91f48
Rehan George <rehan.george@gmail.com>
2025-05-21 13:02:24 +0530

    Add products list

--- ListsApp/ListsApp/Program.cs
+++ ListsApp/ListsApp/Program.cs
@@ -6,6 +6,25 @@ namespace ListsApp
     {
         static void Main(string[] args)
         {
+            List<Product> products = new List<Product> {
+                new Product { Name = "Apple", Price = 0.80 },
+                new Product { Name = "Banana", Price = 0.30 },
+                new Product { Name = "Cherry", Price = 3.80 }
+                };
+
+            products.Add(new Product { Name = "Berries", Price = 2.99 });
+
+            List<Product> cheapProducts = products.Where(p => p.Price < 1.0).ToList();
+
+            Console.WriteLine("Available Products: ");
+
+            foreach (Product product in cheapProducts)
+            {
+                Console.WriteLine($"Product name: {product.Name} for price {product.Price}");
+            }
+
+
+            /* Lists
             // Declaring a list and initializing
             List<string> colors = ["red", "blue", "green", "red"];
             List<int> numbers = new List<int> { 10, 5, 15, 3, 9,  25, 18 };
@@ -33,6 +52,7 @@ namespace ListsApp
             }
 
             printItems(colors);
+            */
         }
 
         private static void printItems<T>(List<T> items)
