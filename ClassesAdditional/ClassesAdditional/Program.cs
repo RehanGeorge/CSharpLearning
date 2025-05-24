4f7d34759dd821b5c2e04a3963a0edc28f02f166
Rehan George <rehan.george@gmail.com>
2025-05-24 20:55:32 +0530

    Initial commit

---
+++ ClassesAdditional/ClassesAdditional/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ClassesAdditional
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+
+            try
+            {
+                Console.WriteLine("Please enter a number");
+                int num1 = int.Parse(Console.ReadLine());
+                int num2 = 0;
+
+                result = num1 / num2;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                Console.WriteLine($"Result: {result}");
+            }
+        }
+    }
+}
