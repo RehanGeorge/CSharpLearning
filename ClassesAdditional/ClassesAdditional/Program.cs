8a0ec443288deb8fb602bd523c2bc1ac7f1e6d26
Rehan George <rehan.george@gmail.com>
2025-05-24 21:11:38 +0530

    Add try catch finally

--- ClassesAdditional/ClassesAdditional/Program.cs
+++ ClassesAdditional/ClassesAdditional/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ClassesAdditional
+﻿using System.Diagnostics;
+
+namespace ClassesAdditional
 {
     internal class Program
     {
@@ -6,19 +8,43 @@
         {
             int result = 0;
 
+            Debug.WriteLine("Main method is running");
+
+            Console.WriteLine("Enter your age:");
+            GetUserAge(Console.ReadLine());
+
             try
             {
                 Console.WriteLine("Please enter a number");
                 int num1 = int.Parse(Console.ReadLine());
-                int num2 = 0;
+                int num2 = 3;
 
                 result = num1 / num2;
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Error: {ex.Message}");
+                // This is only executed during "Debugging"
+                Debug.WriteLine(ex.ToString());
+            }
+            finally
+            {
                 Console.WriteLine($"Result: {result}");
             }
+        }
+
+        static int GetUserAge(string input)
+        {
+            int age;
+            if (!int.TryParse(input, out age))
+            {
+                throw new Exception("You didn't enter a valid age.");
+            }
+            if (age < 0 || age > 120)
+            {
+                throw new Exception("Your age must be between 0 and 120.");
+            }
+            return age;
         }
     }
 }
