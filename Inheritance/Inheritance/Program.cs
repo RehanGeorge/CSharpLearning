4167db5d8bde7988a21061f28822b65117b24eba
Rehan George <rehan.george@gmail.com>
2025-05-27 18:18:19 +0530

    Initial commit

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Eating...");
+        }
+    }
+
+    class Dog: Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("Barking...");
+        }
+    }
+}
