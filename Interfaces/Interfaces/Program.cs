ae6df486927dbfc06c3d8f694eda8cc45853ea9b
Rehan George <rehan.george@gmail.com>
2025-05-29 18:11:48 +0530

    Initial commit

---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Interfaces
+{
+    public interface IAnimal
+    {
+        void MakeSound();
+        void Eat(string food);
+    }
+
+    public class Dog : IAnimal
+    {
+        public void Eat(string food)
+        {
+            Console.WriteLine("Dog ate " + food);
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine("Dog barks");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.MakeSound();
+            dog.Eat("bone");
+        }
+    }
+}
