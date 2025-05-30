ab29ed77b75ebc0c51ac296279064ccfcfbdad7e
Rehan George <rehan.george@gmail.com>
2025-05-30 19:39:37 +0530

    Add structs

---
+++ StructsApp/StructsApp/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace StructsApp
+{
+
+    public struct Point
+    {
+        public double X { get; }
+        public double Y { get; }
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Point: ({X}, {Y})");
+        }
+
+        public double DistanceTo(Point other)
+        {
+            double deltaX = other.X - X;
+            double deltaY = other.Y - Y;
+            return Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(10, 20);
+            p1.Display();
+
+            Point p2 = new Point(20, 30);
+            p2.Display();
+
+            double distance = p1.DistanceTo(p2);
+            Console.WriteLine($"Distance between points: {distance:F2}");
+
+            Point p3 = p1;
+        }
+    }
+}
