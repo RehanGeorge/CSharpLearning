431b171629321c472d0210cb789893c849c6f01c
Rehan George <rehan.george@gmail.com>
2025-06-01 17:51:34 +0530

    Example of delegates

---
+++ DelegatesAndEvents/DelegatesAndEvents/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace DelegatesAndEvents
+{
+    public delegate void LogHandler(string message);
+
+    public class Logger
+    {
+        public void LogToConsole(string message)
+        {
+            Console.WriteLine("Console Log: " + message);
+        }
+
+        public void LogToFile(string message)
+        {
+            Console.WriteLine("File log: " + message);
+        }
+    }
+
+    // 1. Declaration
+    public delegate void Notify(string message);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 2. Instantiation
+            Notify notifyDelegate = ShowMessage;
+            //Notify notifyDelegate = new Notify(ShowMessage);
+
+            // 3. Invocation
+            notifyDelegate("Hello, Delegates!");
+
+            Logger logger = new Logger();
+            LogHandler logHandler = logger.LogToConsole;
+            logHandler("Logging to console.");
+
+            logHandler = logger.LogToFile;
+            logHandler("Logging to file.");
+        }
+
+        static void ShowMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
