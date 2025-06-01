ba6d09d037e3dd356a17640cd74fbfce11cb2e7c
Rehan George <rehan.george@gmail.com>
2025-06-01 19:46:10 +0530

    Complete delegates

--- EventsApp/EventsApp/Program.cs
+++ EventsApp/EventsApp/Program.cs
@@ -1,41 +1,62 @@
 ﻿namespace EventsApp
 {
 
-    public delegate void Notify(string message);
+    //public delegate void Notify(string message);
 
-    public delegate void TemperatureChangeHandler(string message);
+    //public delegate void TemperatureChangeHandler(string message);
+
+    public class TemperatureChangedEventArgs : EventArgs
+    {
+        public int Temperature { get;  }
+        public TemperatureChangedEventArgs(int temperature)
+        {
+            Temperature = temperature;
+        }
+    }
 
     public class TemperatureMonitor
     {
-        public event TemperatureChangeHandler OnTemperatureChanged;
+        //public event TemperatureChangeHandler OnTemperatureChanged;
+
+        public event EventHandler<TemperatureChangedEventArgs> TemperatureChanged;
 
         private int _temperature;
         public int Temperature { get => _temperature;
             set
             {
-                _temperature = value;
-                if(_temperature > 30)
+                if(_temperature != value)
                 {
+                    _temperature = value;
                     // RAISE EVENT
-                    RaiseTemperatureChangedEvent("Temperature exceeded 30 degrees Celsius!");
+                    OnTemperatureChanged(new TemperatureChangedEventArgs(value));
                 }
             }
         }
 
-        protected virtual void RaiseTemperatureChangedEvent(string message)
+        protected virtual void OnTemperatureChanged(TemperatureChangedEventArgs e)
         {
-            OnTemperatureChanged?.Invoke(message);
+            // Letting every subscriber know!
+            TemperatureChanged?.Invoke(this, e);
         }
     }
 
     public class TemperatureAlert
     {
-        public void OnTemperatureChanged(string message)
+        public void OnTemperatureChanged(object sender, TemperatureChangedEventArgs e)
         {
-            Console.WriteLine($"Alert: {message}");
+            Console.WriteLine($"Alert: temperature is {e.Temperature} sender is: {sender} ");
         }
     }
 
+    public class TempCoolingAlert
+    {
+        public void OnTemperatureChanged(object sender, TemperatureChangedEventArgs e)
+        {
+            Console.WriteLine($"TEMP Cooling Alert: temperature is {e.Temperature} sender is: {sender} ");
+        }
+    }
+
+    /*
     public class EventPublisher
     {
         public event Notify OnNotify;
@@ -53,19 +74,25 @@
             Console.WriteLine($"Event received with message: {message}");
         }
     }
+    */
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            /*
             EventPublisher publisher = new EventPublisher();
             EventSubscriber subscriber = new EventSubscriber();
             publisher.OnNotify += subscriber.OnEventRaised;
 
             publisher.RaiseEvent("This is a test event message.");
+            */
 
             TemperatureMonitor monitor = new TemperatureMonitor();
             TemperatureAlert alert = new TemperatureAlert();
-            monitor.OnTemperatureChanged += alert.OnTemperatureChanged;
+            TempCoolingAlert alert2 = new TempCoolingAlert();
+            monitor.TemperatureChanged += alert.OnTemperatureChanged;
+            monitor.TemperatureChanged += alert2.OnTemperatureChanged;
 
             monitor.Temperature = 32; // This will trigger the alert
             monitor.Temperature = 28; // This will not trigger the alert
