9282d4b477706bf360154fd4f48d8303d0bb34a9
Rehan George <rehan.george@gmail.com>
2025-06-04 18:25:52 +0530

    Create To do App

--- TodoApp/TodoApp/MainWindow.xaml.cs
+++ TodoApp/TodoApp/MainWindow.xaml.cs
@@ -23,7 +23,21 @@ namespace TodoApp
 
         private void AddTodoButton_Click(object sender, RoutedEventArgs e)
         {
-            
+            string todoText = TodoInput.Text;
+
+            if(!string.IsNullOrEmpty(todoText))
+            {
+                TextBlock todoItem = new TextBlock
+                {
+                    Text = todoText,
+                    Margin = new Thickness(10),
+                    Foreground = new SolidColorBrush(Colors.White)
+                };
+
+                TodoList.Children.Add(todoItem);
+
+                TodoInput.Clear();
+            } 
         }
     }
 }
