990cd3484c705b59b48ffb394bc6fdff070fff4f
Rehan George <rehan.george@gmail.com>
2025-06-05 18:52:21 +0530

    Add LoginView

--- WpfDemo/WpfDemo/LoginView.xaml.cs
+++ WpfDemo/WpfDemo/LoginView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,15 +16,18 @@ using System.Windows.Shapes;
 namespace WpfDemo
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for LoginView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginView : UserControl
     {
-        public MainWindow()
+        public LoginView()
         {
             InitializeComponent();
+        }
 
-            MainContent.Content = new LoginView();
+        private void LoginButton_Clicked(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
