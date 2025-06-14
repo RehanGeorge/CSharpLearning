e3817c42bbabdd1feb4abfbbb5b0fdfe0e730765
Rehan George <rehan.george@gmail.com>
2025-06-14 21:57:35 +0530

    Restart project

--- WPF Zoo Manager/WPF Zoo Manager/App.xaml.cs
+++ WPF Zoo Manager/WPF Zoo Manager/App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Data;
 using System.Windows;
 
-namespace WPF_ZooManager
+namespace WPF_Zoo_Manager
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- WPF Zoo Manager/WPF Zoo Manager/MainWindow.xaml.cs
+++ WPF Zoo Manager/WPF Zoo Manager/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WPF_ZooManager
+namespace WPF_Zoo_Manager
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
