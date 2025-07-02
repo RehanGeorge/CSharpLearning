9190ba652d886851c30068e140a5ecdef2b1e45d
Rehan George <rehan.george@gmail.com>
2025-07-02 19:56:47 +0530

    Create Linq function

---
+++ Linq1/Linq1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            OddEvenNumbers(numbers);
+
+        }
+
+        static void OddEvenNumbers(int[] numbers)
+        {
+            IEnumerable<int> oddNumbers = numbers.Where(n => n % 2 != 0);
+            Console.WriteLine($"Odd Numbers: {string.Join(", ", oddNumbers)}");
+            IEnumerable<int> evenNumbers = from number in numbers where number % 2 == 0 select number;
+            //Console.WriteLine($"Odd Numbers: {oddNumbers}");
+            Console.WriteLine($"Even Numbers: {string.Join(", ", evenNumbers)}");
+        }
+    }
+}
