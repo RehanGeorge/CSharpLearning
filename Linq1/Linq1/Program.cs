c31e5837e8806cfd2a207ea73bbfd4fa64759ecc
Rehan George <rehan.george@gmail.com>
2025-07-03 19:28:02 +0530

    Add a bunch of stuff

--- Linq1/Linq1/Program.cs
+++ Linq1/Linq1/Program.cs
@@ -30,4 +30,50 @@ namespace Linq1
             }
         }
     }
+
+    class UniversityManager
+    {
+        public List<University> universities;
+        public List<Student> students;
+
+        public UniversityManager()
+        {
+            universities = new List<University>();
+            students = new List<Student>();
+
+            universities.Add(new University { Id = 1, Name = "University A" });
+            universities.Add(new University { Id = 2, Name = "University B" });
+
+            students.Add(new Student { Id = 1, Name = "Alice", Gender = "female", Age = 20, UniversityId = 1 });
+            students.Add(new Student { Id = 2, Name = "Bob", Gender = "male", Age = 20, UniversityId = 1 });
+            students.Add(new Student { Id = 3, Name = "Charlie", Gender = "male", Age = 22, UniversityId = 2 });
+            students.Add(new Student { Id = 4, Name = "Dave", Gender = "male", Age = 21, UniversityId = 2 });
+            students.Add(new Student { Id = 5, Name = "Ennui", Gender = "female", Age = 23, UniversityId = 1 });
+        }
+    }
+    class University
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine("University {0} with id {1}", Name, Id);
+        }
+    }
+
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+
+        // Foreign Key
+        public int UniversityId { get; set; }
+        public void Print()
+        {
+            Console.WriteLine("Student {0} with Id {1}, Gender {2} and Age {3} from University with the Id {4}", Name, Id, Gender, Age, UniversityId);
+        }
+    }
 }
