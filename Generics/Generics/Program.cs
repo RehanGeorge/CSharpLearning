21e59d2a869190aafb34a7bdb51713e2c4da9815
Rehan George <rehan.george@gmail.com>
2025-07-13 19:47:35 +0530

    Add generics

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<int> intBox = new Box<int>();
+            intBox.Content = 42;
+            Console.WriteLine(intBox.Log());
+
+            Box<string> stringBox = new Box<string>();
+            stringBox.Content = "Hello, Generics!";
+            Console.WriteLine(stringBox.Log());
+        }
+    }
+}
