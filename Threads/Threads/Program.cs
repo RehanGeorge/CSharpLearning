17f48cfe1365766f71c9da34fcdbf190e14c1a98
Rehan George <rehan.george@gmail.com>
2025-07-20 18:53:59 +0530

    Add threads ending at different times

--- Threads/Threads/Program.cs
+++ Threads/Threads/Program.cs
@@ -6,15 +6,52 @@ namespace Threads
     {
         static void Main(string[] args)
         {
-            Enumerable.Range(0, 1000).ToList().ForEach(f =>
+
+            Console.WriteLine("Main Thread started");
+            Thread thread1 = new Thread(Thread1Function);
+            Thread thread2 = new Thread(Thread2Function);
+            thread1.Start();
+            thread2.Start();
+
+            if(thread1.Join(1000))
             {
-                new Thread(() =>
+                Console.WriteLine("Thread1Function done");
+            }
+            else
+            {
+                Console.WriteLine("Thread1Function wasn't done within 1 sec");
+            }
+
+            thread2.Join();
+            Console.WriteLine("Thread2Function done");
+
+            Console.WriteLine("Main Thread ended");
+
+            /*
+            Enumerable.Range(0, 100).ToList().ForEach(f =>
+            {
+                ThreadPool.QueueUserWorkItem((o) =>
                 {
-                    Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} started");
+                    Console.WriteLine($"Thread number: {Environment.CurrentManagedThreadId} started");
                     Thread.Sleep(1000); // Simulate some work
-                    Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} finished");
-                }).Start();
+                    Console.WriteLine($"Thread number: {Environment.CurrentManagedThreadId} finished");
+                });
             });
+
+            Console.ReadLine();
+            */
+        }
+
+        public static void Thread1Function()
+        {
+            Console.WriteLine("Thread1 Function started");
+            Thread.Sleep(2000);
+            Console.WriteLine("Thread1Function coming back to caller");
+        }
+
+        public static void Thread2Function()
+        {
+            Console.WriteLine("Thread2 Function started");
         }
     }
 }
