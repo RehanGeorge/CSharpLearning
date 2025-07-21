d279f4f136629d2513d3a6606452bd9c06627f6f
Rehan George <rehan.george@gmail.com>
2025-07-21 19:30:28 +0530

    Add tests and API

---
+++ Calculator/Web/Controllers/CalculateController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Domain;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculateController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<CalculateController> _logger;
+
+        public CalculateController(ILogger<CalculateController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("Add/{left}/{right}")]
+        public int Get(int left, int right)
+        {
+            var calculator = new Calculator();
+            return calculator.Sum(left, right);
+        }
+    }
+}
