e1d2cadc242e97ac830fa8245eac60fed5bd6516
Rehan George <rehan.george@gmail.com>
2025-07-21 19:21:28 +0530

    Create new project state

--- Calculator/Calculator/UnitTest1.cs
+++ Calculator/Calculator/UnitTest1.cs
@@ -1,20 +1,20 @@
-namespace Calculator
+using Domain;
+
+namespace CalculatorTest
 {
     public class UnitTest1
     {
         [Fact]
         public void Test1()
         {
-            if (Sum(2, 3) != 5)
+            var calculator = new Calculator();
+            if (calculator.Sum(2, 3) != 5)
             {
                 throw new Exception("Sum(2, 3) should be 5");
             }
 
         }
 
-        int Sum(int left, int right)
-        {
-            return left + right;
-        }
+
     }
 }
