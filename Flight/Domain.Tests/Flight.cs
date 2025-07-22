94388409894920a5d3a49ec07272fde0783efa80
Rehan George <rehan.george@gmail.com>
2025-07-22 20:55:02 +0530

    Create New Project

---
+++ Flight/Domain.Tests/Flight.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Tests;
+
+public class Flight
+{
+    public int SeatCapacity { get; set; }
+    public int RemainingNumberOfSeats { get; set; }
+    public Flight(int seatCapacity)
+    {
+        SeatCapacity = seatCapacity;
+        RemainingNumberOfSeats = seatCapacity;
+    }
+
+    public void Book(string v1, int numberOfSeats)
+    {
+        RemainingNumberOfSeats -= numberOfSeats;
+    }
+}
