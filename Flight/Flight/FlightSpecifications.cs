3ea5d5ddc85cda0313bc1e777324dd9d9bbaf06d
Rehan George <rehan.george@gmail.com>
2025-07-26 19:14:08 +0530

    Increase trustworthiness by adding additional tests

--- Flight/Flight/FlightSpecifications.cs
+++ Flight/Flight/FlightSpecifications.cs
@@ -15,6 +15,22 @@ namespace FlightTest
         }
 
         [Fact]
+        public void Booking_reduces_the_number_of_seats_by_the_number_of_seats_booked_2()
+        {
+            var flight = new Flight(seatCapacity: 5);
+            flight.Book("rehan", 3);
+            flight.RemainingNumberOfSeats.Should().Be(2);
+        }
+
+        [Fact]
+        public void Booking_reduces_the_number_of_seats_by_the_number_of_seats_booked_3()
+        {
+            var flight = new Flight(seatCapacity: 50);
+            flight.Book("rehan", 24);
+            flight.RemainingNumberOfSeats.Should().Be(26);
+        }
+
+        [Fact]
         public void Avoid_Overbooking()
         {
             // Given
@@ -25,6 +41,18 @@ namespace FlightTest
 
             // Then
             error.Should().BeOfType<OverbookingError>();
+        }
+
+        [Fact]
+        public void Books_flights_successfully()
+        {
+            // Given
+            var flight = new Flight(seatCapacity: 3);
+            // When
+            var error = flight.Book("rehan", 2);
+            // Then
+            error.Should().BeNull();
+            flight.RemainingNumberOfSeats.Should().Be(1);
         }
     }
 }
