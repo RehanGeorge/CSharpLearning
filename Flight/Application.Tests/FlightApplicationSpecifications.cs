d86dc25f5c4de7b3485221668a24d3f234a2a122
Rehan George <rehan.george@gmail.com>
2025-07-30 19:32:59 +0530

    Move references to the right places

--- Flight/Application.Tests/FlightApplicationSpecifications.cs
+++ Flight/Application.Tests/FlightApplicationSpecifications.cs
@@ -2,6 +2,7 @@ using FluentAssertions;
 using Data;
 using Domain;
 using Microsoft.EntityFrameworkCore;
+using Application.Tests;
 
 namespace Application.Tests
 {
@@ -25,56 +26,6 @@ namespace Application.Tests
             bookingService.FindBookings(flight.Id).Should().ContainEquivalentOf(
                 new BookingRm(passengerEmail, numberOfSeats)
                 );
-        }
-    }
-
-    public class BookingService
-    {
-        public Entities Entities { get; set; }
-        public BookingService(Entities entities)
-        {
-            Entities = entities;
-        }
-        public void Book(BookDto bookDto)
-        {
-            var flight = Entities.Flights.Find(bookDto.FlightId);
-            flight.Book(bookDto.PassengerEmail, bookDto.NumberOfSeats);
-            Entities.SaveChanges();
-        }
-
-        public IEnumerable<BookingRm> FindBookings(Guid flightId)
-        {
-            return Entities.Flights
-                .Find(flightId)
-                .BookingList
-                .Select(booking => new BookingRm(
-                    booking.Email,
-                    booking.NumberOfSeats
-                    ));
-        }
-    }
-
-    public class BookDto
-    {
-        public Guid FlightId { get; set; }
-        public string PassengerEmail { get; set; }
-        public int NumberOfSeats { get; set; }
-        public BookDto(Guid flightId, string passengerEmail, int numberOfSeats)
-        {
-            FlightId = flightId;
-            PassengerEmail = passengerEmail;
-            NumberOfSeats = numberOfSeats;
-        }
-    }
-
-    public class BookingRm
-    {
-        public string PassengerEmail { get; set; }
-        public int NumberOfSeats { get; set; }
-        public BookingRm(string passengerEmail, int numberOfSeats)
-        {
-            PassengerEmail = passengerEmail;
-            NumberOfSeats = numberOfSeats;
         }
     }
 }
